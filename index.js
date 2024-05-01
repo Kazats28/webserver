f3115b6946231ff3e30bd7dbe15af677d391f543
Kazats28 <krtasn2003@gmail.com>
2024-05-01 23:41:45 +0700

    make it better

--- index.js
+++ index.js
@@ -19,11 +19,14 @@ app.use("/booking", bookingRoute);
 app.use("/favorite", favoriteRoute);
 app.use("/rate", rateRoute);
 
+const PORT = process.env.PORT_ENV || 8282;
+
+
 mongoose.connect(`mongodb+srv://lynk64te:${process.env.MONGODB_PASSWORD}@cluster0.zlxkken.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 ).then(() => {
   console.log("Mongodb connected");
-  app.listen(process.env.PORT_ENV || 8282, () => {
-    console.log(`Server is listening on port ${process.env.PORT_ENV}`);
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
   });
 }).catch((err) => {
   console.log({ err });
